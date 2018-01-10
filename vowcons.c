07346d3eab895356022c2af2d2ad3e07db8c4a3d
preethipriy <34617823+preethipriy@users.noreply.github.com>
2018-01-10 16:42:00 +0530

    Create vowcons.c

---
+++ vowcons.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<conio.h>
+void main() {
+char ch;
+printf("enter the number :%s",ch);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='u')
+{
+	printf("vowel");
+	
+}
+else
+{
+	printf("consonent");
+}
+getch();
+}
