6fa9546ab6763551aac403a59bb74bea35dec7c5
preethipriy <34617823+preethipriy@users.noreply.github.com>
2018-01-10 16:29:00 +0530

    Create poneze.c

---
+++ poneze.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+int a;
+printf("enter the number :%d",a);
+if(a<=0)
+{
+if(a==0)
+{
+	printf("zero");
+ }
+else
+{
+	printf("negative");
+}
+}
+else
+{
+printf("positive");
+}
+return 0;
+}
