1279d9e91e94a8fd264a0b8f01e114713013e134
preethipriy <34617823+preethipriy@users.noreply.github.com>
2018-01-19 16:24:18 +0530

    Create digit.c

---
+++ digit.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,count=0;
+printf("enter the number");
+scanf("%d",&n);
+while(n!=0)
+{
+n=n/10;
+count++;
+}
+printf("digits in an number is");
+scanf("%d",&count);
+}
